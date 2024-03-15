ccca4747224da02e58431e2a2ac5f62a3cb9008a
Alleph Nogueira <allephn@hotmail.com.br>
2024-03-15 11:27:27 -0300

    Criando o logs.txt | Colocando mais detalhes no codigo 1.0

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function sortear(){
-    let quantidade = document.getElementById('quantidade').value;
+    let quantidade = document.getElementById('quantidade').value; // Pegando o valor que foi digitado
 
-    let de = document.getElementById('de').value;
+    let de = document.getElementById('de').value; // Pegando o valor que foi digitado
 
-    let ate = document.getElementById('ate').value;
+    let ate = document.getElementById('ate').value; // Pegando o valor que foi digitado
 
     console.log(`quantidade: ${quantidade} do numero: ${de} ate o numero ${ate}`);
 }
