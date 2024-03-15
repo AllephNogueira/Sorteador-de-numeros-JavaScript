589b8843a97a994a7529eda508cb98317c1accc3
Alleph Nogueira <allephn@hotmail.com.br>
2024-03-15 11:31:32 -0300

    Todas as variaveis recebem numeros inteiros.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function sortear(){
-    let quantidade = document.getElementById('quantidade').value; // Pegando o valor que foi digitado
+    let quantidade = parseInt(document.getElementById('quantidade').value); // Pegando o valor que foi digitado
 
-    let de = document.getElementById('de').value; // Pegando o valor que foi digitado
+    let de = parseInt(document.getElementById('de').value); // Pegando o valor que foi digitado
 
-    let ate = document.getElementById('ate').value; // Pegando o valor que foi digitado
+    let ate = parseInt(document.getElementById('ate').value); // Pegando o valor que foi digitado
 
     console.log(`quantidade: ${quantidade} do numero: ${de} ate o numero ${ate}`);
 }
