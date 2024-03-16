cf3c43554fe6afaa4257304619a0185155ad80f3
Alleph Nogueira <allephn@hotmail.com.br>
2024-03-16 12:52:23 -0300

    Agora nao adiciona mais numeros sorteados iguais

--- app.js
+++ app.js
@@ -7,19 +7,41 @@ function sortear(){
     let ate = parseInt(document.getElementById('ate').value); // Pegando o valor que foi digitado
 
     
-    let numerosSorteado = [] // Array vazio com os numeros sorteados. 
+    let numerosSorteado = []; // Array vazio com os numeros sorteados. 
     let numero;
+
 
     for (let contador = 0; contador < quantidade; contador++){
 
         numero = obterNumerosAleatorios(de, ate);
+
+        console.log(`numero fora do while ${numero}`)
+
+        while(numerosSorteado.includes(numero)){
+            // Includes retorna um false ou true
+            // Se dentro do numero sorteado ja estiver o NUMERO NOVO
+            // Ele retorna um TRUE e sortea outro numero
+
+                numero = obterNumerosAleatorios(de, ate);
+
+                console.log(`numero dentro do while ${numero}`)
+
+        }
+        
         numerosSorteado.push(numero);
+
+
+        
 
     }
 
-    Mensagens('resultado', numerosSorteado)
+    Mensagens('resultado', numerosSorteado) // Aqui e onde vai aparecer os numeros sorteados.
 
-    console.log(numerosSorteado);
+    // Agora vamos iniciar o reiniciar
+
+
+
+    
 }
 
 
@@ -33,6 +55,10 @@ function obterNumerosAleatorios(min, max){ // mim = numero minimo | max = numero
 }
 function Mensagens(id, texto){
 
-    document.getElementById(id).innerHTML = `Numeros sorteados foram ${texto}`;
+    document.getElementById(id).innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${texto}</label>`;
+
+}
+
+function reiniciarJogos(){
 
 }
